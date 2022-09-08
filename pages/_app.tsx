225f60616208496c74e0c20918d20183b821fa57
LBruner <leandrobruner15@gmail.com>
2022-09-07 21:49:04 -0300

    add head tag information

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {Provider} from "react-redux";
 import storeIndex from "../src/components/store/StoreIndex";
-import Navbar from "../src/components/UI/Navbar"; 
+import Navbar from "../src/components/UI/Navbar";
+import Head from "next/head"; 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>    
+    <Head>
+      <title>Ankimate</title>
+      <link rel="shortcut icon" href="/favicon.png" />
+      <meta name="description" content="Anki automated cards"/>
+      <meta name={"viewport"} content={"initial-scale=1.0, width=device-width"} />
+    </Head>
     <Provider store={storeIndex}>
     <Navbar/>
     <Component {...pageProps} />
