b881ba6d4d79e88176db0e8b09194f2dc7c9dad5
LBruner <leandrobruner15@gmail.com>
2023-01-16 11:46:16 -0300

    fix waiting modal animation

--- src/UI/WaitScreen.tsx
+++ src/UI/WaitScreen.tsx
@@ -1,10 +1,18 @@
 import {useSelector} from "react-redux";
 import {RootState} from "../store/StoreIndex";
 import classes from './WaitScreen.module.css';
-import React from "react";
+import React, {useEffect} from "react";
 
 const WaitScreen: React.FC = () => {
     const isWaiting: boolean = useSelector((state: RootState) => state.ui.isWaiting);
+
+    useEffect(() => {
+        if (!isWaiting)
+            window.scrollTo({
+                top: window.scrollY + window.innerHeight,
+                behavior: 'smooth'
+            });
+    }, [isWaiting]);
     
     return (
         <>
